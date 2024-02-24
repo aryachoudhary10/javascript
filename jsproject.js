b4b5579b5adcf4f93b0e46a397dc17edbab32d69
Arya <111578761+aryachoudhary10@users.noreply.github.com>
2024-02-25 00:17:45 +0530

    Create jsproject.js

---
+++ jsproject.js
@@ -0,0 +1,39 @@
+const NFTs= [];
+
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (_name, _eyeColor, _shirtType, _bling) {
+      const NFT= {
+         "name": _name,
+         "eyeColor": _eyeColor,
+         "shirtType": _shirtType,
+         "bling": _bling
+      }
+      NFTs.push(NFT);
+      console.log("Minted NFT is:"+ _name);
+}
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+   for(let i=0; i<NFTs.length; i++){
+      console.log("\nID: "+ i);
+      console.log("Name: " +NFTs[i].name);
+      console.log("Eye color: "+NFTs[i].eyeColor);
+      console.log("Shirt type: "+NFTs[i].shirtType);
+      console.log("Bling: "+NFTs[i].bling);
+   }
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+   console.log("\nTotal NFTs minted is: "+NFTs.length);
+}
+
+// call your functions below this line
+mintNFT("Harsh", "Black", "Tshirt", "Gold chain");
+mintNFT("Sonu", "Black", "Check shirt", "Silver chain");
+mintNFT("Atishi", "Brown", "Hoodie", "Diamond ring");
+listNFTs();
+getTotalSupply();
